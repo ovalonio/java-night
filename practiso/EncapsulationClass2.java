70b46a6abef7d92fd51dec22f65994a941bffd8d
ovalonio <ovalonio@gmail.com>
2025-05-26 15:23:35 +0000

    Encapsulation

--- practiso/EncapsulationClass2.java
+++ practiso/EncapsulationClass2.java
@@ -1,12 +1,12 @@
 package practiso;
 
-import practiso.LambdaClass;
+import practiso.EncapsulationClass;
 
 public class EncapsulationClass2 {
     public static void main(String[] args) {
         
     
-    practiso.LambdaClass lc = new practiso.LambdaClass();
+    practiso.EncapsulationClass lc = new practiso.EncapsulationClass();
     Department dept = new Department();
     dept.setIT("IT Tom");;
     System.out.println(dept.getIT());
