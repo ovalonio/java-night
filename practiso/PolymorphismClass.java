e03e01fbb31920fd59c956e50b0b6dd6cf40096e
ovalonio <ovalonio@gmail.com>
2025-05-26 16:30:19 +0000

    Polymorphism

--- practiso/PolymorphismClass.java
+++ practiso/PolymorphismClass.java
@@ -2,16 +2,21 @@ package practiso;
 
 public class PolymorphismClass {
     public static void main(String[] args) {
+        HR.Training_And_Development(new Sales());
+            
+        
         
     }
 }
 
 class Department{
-
+    public String getName(){
+        return getClass().getSimpleName();
+    }
 }
 
 class IT extends Department{
-
+    
 }
 
 class Sales extends Department{
@@ -32,4 +37,10 @@ class Manage_IT_Ticket implements IT_Support, HelpDesk{
 
 class Infrastructure implements IT_Support{
 
+}
+
+class HR{
+    public static void Training_And_Development(Department department){
+        System.out.println("List of new trainee: " + department.getName());
+    }
 }
