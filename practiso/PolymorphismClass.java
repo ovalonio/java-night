f6de8a90e1e24283822c3af3f70b6ac69869bd77
ovalonio <ovalonio@gmail.com>
2025-05-26 15:33:37 +0000

    Polymorphism

---
+++ practiso/PolymorphismClass.java
@@ -0,0 +1,28 @@
+package practiso;
+
+public class PolymorphismClass {
+    public static void main(String[] args) {
+        
+    }
+}
+
+class Department{
+
+}
+
+class IT extends Department{
+
+}
+
+class Sales extends Department{
+
+}
+
+interface IT_Support{
+    
+}
+
+class Manage_IT_Ticket implements IT_Support{
+
+}
+
