4dab0d46fd3533af864a35446ac75e84c63f1aa8
ovalonio <ovalonio@gmail.com>
2025-06-07 16:38:35 +0000

    MultiplicationTable

---
+++ ProgrammingChallenge/MultiplicationTable.java
@@ -0,0 +1,26 @@
+// Multiplication Table 10x10
+
+package ProgrammingChallenge;
+
+public class MultiplicationTable {
+    public static void main(String[] args) {
+        new PrepareTable();
+    }
+}
+
+class PrepareTable{
+    public PrepareTable(){
+        int row = 10;
+        int w = 1;
+        int y;
+
+        for (int x = 1 ; x <= row ; x++){
+            //System.out.println("(x: " +x+ ")");
+            for (y = 1; y <= row ; y++, w += x){
+                System.out.print(" " + w + " ");
+            }
+            w = x + 1;
+            System.out.println();
+        }
+    }
+}
