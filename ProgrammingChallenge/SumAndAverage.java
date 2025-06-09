2864524caee958f6d2f604abee7ce0b0b88b0672
ovalonio <ovalonio@gmail.com>
2025-06-09 05:05:15 +0000

    SumAndAverage

---
+++ ProgrammingChallenge/SumAndAverage.java
@@ -0,0 +1,37 @@
+//Sum and average of input array numbers
+
+package ProgrammingChallenge;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class SumAndAverage{
+    public static void main(String[] args) {
+        new Input();
+    }
+}
+
+class Input{
+    public Input(){
+        int[] number = new int[3];
+        int sumNum = 0;
+        double aveSumNum;
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("'Type numbers to get the average'");
+        for (int y =0 ; y <= 2 ; y++ ){
+            System.out.print("Enter number: ");
+            number[y] = sc.nextInt();
+        }
+        sc.close();
+
+        for (int x: number){
+            sumNum += x;
+        }
+        System.out.println("====================");
+        System.out.println("Sum of numbers: " + sumNum);
+        aveSumNum = sumNum / number.length;
+        System.out.println("The average is: " + aveSumNum);
+
+    }
+}
