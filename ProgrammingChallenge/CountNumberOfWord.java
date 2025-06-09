69c72980c6e96a27139388dafb9599075df21b4c
ovalonio <ovalonio@gmail.com>
2025-06-09 01:19:44 +0000

    CountNumberOfWord

---
+++ ProgrammingChallenge/CountNumberOfWord.java
@@ -0,0 +1,20 @@
+//Count number of words in an input
+
+package ProgrammingChallenge;
+
+import java.util.Scanner;
+
+public class CountNumberOfWord {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.print("Type a short paragraph or sentence: ");
+        String z = sc.nextLine();
+
+        String[] x = z.trim().split("\\s+");
+        System.out.print("The number of word you have put in: ");
+        int y = x.length;
+        System.out.println(y);
+
+    }
+}
